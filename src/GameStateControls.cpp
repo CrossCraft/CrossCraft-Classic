f1ccdabee1e83c0aec72f56da518538d460ef751
Fighter19 <1475802+Fighter19@users.noreply.github.com>
2022-08-10 16:54:17 +0200

    Bind mouse wheel to cycle items

--- src/GameStateControls.cpp
+++ src/GameStateControls.cpp
@@ -212,6 +212,14 @@ void GameState::bind_controls() {
         {(int)Input::Keys::Num9, KeyFlag::Press | KeyFlag::Held},
         {Player::change_selector, SelData{world->player.get(), 8}});
 
+    // Mouse wheel to cycle through objects
+    mouse_controller->add_command(
+        {(int)Input::MouseButtons::MWheelDown, KeyFlag::Press},
+        {Player::press_left, world->player.get()});
+    mouse_controller->add_command(
+        {(int)Input::MouseButtons::MWheelUp, KeyFlag::Press},
+        {Player::press_right, world->player.get()});
+
     // Map directions to numpad
     key_controller->add_command(
         {(int)Input::Keys::KeyPad8, KeyFlag::Press},
